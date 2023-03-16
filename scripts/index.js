1f206f5c545d3afdd96b96d3e2feb7dbcd12d5a5
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-16 07:29:54 +0200

    Made the header sticky on scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,27 @@
-const menuIcon = document.querySelector(".menu-icon");
+const header = document.querySelector(".main__header");
 const navbar = document.querySelector(".header__nav");
+const menuIcon = document.querySelector(".menu-icon");
 
 const sliderControls = document.querySelector(".slider__controls");
 const slider = document.querySelector(".slider__inner");
 
+window.addEventListener("scroll", () => {
+  // header.classList.toggle("scrolled");
+  document.body.classList.toggle("scrolled", scrollY > 200);
+});
+
+// Navbar mobile menu
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("header__nav--active");
 });
 
+// Method slider
 sliderControls.addEventListener("click", (e) => {
   if (e.target.closest(".dot")) {
     const index = +e.target.dataset.index;
     const lastActive = sliderControls.querySelector(".dot--active");
 
-    slider.style.translate = `${100 * index}%`;
+    slider.style.translate = `calc(${100 * index}% + 1rem)`;
     lastActive.classList.remove("dot--active");
     e.target.classList.add("dot--active");
   }
