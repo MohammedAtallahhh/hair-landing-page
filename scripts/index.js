66ec69057d4335a8d250b6c67217f439096d4a7d
Mohammed Atallah <mohamedatallahh2017@gmail.com>
2023-03-16 10:25:17 +0200

    Added results section

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,13 @@ const header = document.querySelector(".main__header");
 const navbar = document.querySelector(".header__nav");
 const menuIcon = document.querySelector(".menu-icon");
 
-const sliderControls = document.querySelector(".slider__controls");
-const slider = document.querySelector(".slider__inner");
+// const sliderControls = document.querySelector(".slider__controls");
+const methodSlider = document.querySelector(".method__slider .slider__inner");
+const methodSliderControls = document.querySelector(
+  ".method__slider .slider__controls"
+);
 
+// Make the header sticky on scroll
 window.addEventListener("scroll", () => {
   // header.classList.toggle("scrolled");
   document.body.classList.toggle("scrolled", scrollY > 200);
@@ -15,14 +19,40 @@ menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("header__nav--active");
 });
 
-// Method slider
-sliderControls.addEventListener("click", (e) => {
-  if (e.target.closest(".dot")) {
-    const index = +e.target.dataset.index;
-    const lastActive = sliderControls.querySelector(".dot--active");
+// Slider
+const slideTo = (index, slider, controls) => {
+  const lastActive = controls.querySelector(".dot--active");
+  const newActive = controls.querySelector(`.dot[data-index="${index}"]`);
 
-    slider.style.translate = `calc(${100 * index}% + 1rem)`;
-    lastActive.classList.remove("dot--active");
-    e.target.classList.add("dot--active");
+  slider.style.translate = `${100 * index}%`;
+  lastActive.classList.remove("dot--active");
+  newActive.classList.add("dot--active");
+};
+
+let i = 1;
+let slidesCount = 4;
+let intervalTime = 4000;
+
+const sliderInterval = setInterval(() => {
+  if (i > slidesCount - 1) {
+    i = 0;
   }
+  slideTo(i++, methodSlider, methodSliderControls);
+}, intervalTime);
+
+document.addEventListener("click", (e) => {
+  clearInterval(sliderInterval);
+
+  const slider = e.target.closest(".slider").querySelector(".slider__inner");
+  const controls = e.target.closest(".slider__controls");
+  if (!controls) return;
+
+  const newActive = e.target.closest(".dot");
+  if (!newActive) return;
+
+  const index = +newActive.dataset.index;
+
+  slideTo(index, slider, controls);
 });
+
+document.addEventListener("unload", () => clearInterval(sliderInterval));
